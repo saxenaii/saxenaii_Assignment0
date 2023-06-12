6cf48aad7534229b0d6de9059a8145cfe4e7b8e1
saxenaii <saxenaii@mail.uc.edu>
2023-06-12 01:44:59 -0400

    Created project

---
+++ src/main/java/com/saxenaii_Assignment0/saxenaii_Assignment0/SaxenaiiAssignment0Application.java
@@ -0,0 +1,13 @@
+package com.saxenaii_Assignment0.saxenaii_Assignment0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaxenaiiAssignment0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaxenaiiAssignment0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/saxenaii_Assignment0/saxenaii_Assignment0/SaxenaiiAssignment0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saxenaii_Assignment0.saxenaii_Assignment0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaxenaiiAssignment0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
