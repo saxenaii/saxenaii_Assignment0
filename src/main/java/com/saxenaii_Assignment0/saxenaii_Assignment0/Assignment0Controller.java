6b2054b3364f2fb4dc798a7d1050621da579b1b4
saxenaii <saxenaii@mail.uc.edu>
2023-06-12 02:10:46 -0400

    Created controller class

---
+++ src/main/java/com/saxenaii_Assignment0/saxenaii_Assignment0/Assignment0Controller.java
@@ -0,0 +1,13 @@
+package com.saxenaii_Assignment0.saxenaii_Assignment0;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Assignment0Controller {
+
+  @RequestMapping("/start")
+  public String helloWorld(){
+    return "start";
+  }
+}
